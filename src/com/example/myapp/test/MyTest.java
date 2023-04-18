4ff69ded0caf2a670fe0ec37b6f4a5763126f3cb
Noah Gram <noah@gramonline.nl>
2023-04-18 12:30:11 +0200

    Test added

--- src/com/example/myapp/test/MyTest.java
+++ src/com/example/myapp/test/MyTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 public class MyTest {
     @Test
     void testCustomerTypes() {
@@ -24,5 +26,29 @@ public class MyTest {
         BoatAssembly boatAssembly = new BoatAssembly();
         boatAssembly.selectedBoat = new Boat(boatType);
         boatAssembly.boatOfferte(new Customer());                                                                       
+    }
+
+    @Test
+    void CreateNewBoat() {
+        ArrayList<BoatType> boatTypes = new ArrayList<BoatType>();
+        MotorType Honda = new MotorType("Honda", 5, 500);
+        RoomType stateroon = new RoomType("Stateroom", 4, 1100);
+        SteeringWheelType wood = new SteeringWheelType("Wood", 3, 450);
+        ExhaustType doublePipe = new ExhaustType("Double Pipe", 2, 400);
+        ColorType pearlescent = new ColorType("Pearlescent", 2, 1000);
+        WaterType swimmingPool = new WaterType("Swimming Pool", 2, 4500);
+        FlagType medium = new FlagType("Medium", 2, 60);
+        AnchorType hook = new AnchorType("Hook", 2, 30);
+
+        BoatType boatType = new BoatType("Tester", 1, 1500000, "Medium");
+        boatTypes.add(boatType);
+        boatType.AddMotor(Honda);
+        boatType.AddRoom(stateroon);
+        boatType.AddSteeringWheel(wood);
+        boatType.AddExhaust(doublePipe);
+        boatType.AddColor(pearlescent);
+        boatType.AddWater(swimmingPool);
+        boatType.AddFlag(medium);
+        boatType.AddAnchor(hook);
     }
 }
