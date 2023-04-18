ef70d140e02b4c50b27f45812feb6892294f8003
Noah Gram <noah@gramonline.nl>
2023-04-18 21:37:52 +0200

    Added Junit Tests

--- src/com/example/myapp/test/MyTest.java
+++ src/com/example/myapp/test/MyTest.java
@@ -12,43 +12,57 @@ public class MyTest {
         String expected = "Private";
         assertEquals(expected, actual);
     }
-
     @Test
-    void ExhaustTypePickerTest() {
-        BoatType boatType = new BoatType("Test", 1, 0, "Slow");
-        ExhaustTypePicker exhaustTypePicker = new ExhaustTypePicker();
-        exhaustTypePicker.selectExhaust(boatType);
+    void testExhaustTypePicker() {
+        Boat boat = null;
+        BoatList boatList = new BoatList();
+        int boatTypeName = 11; // Use a boat type number that exists in the BoatList
+        for (BoatType boatType : boatList.getBoatTypes()) {
+            if (boatType.getNumber() == boatTypeName) {
+                boat = new Boat(boatType);
+                break;
+            }
+        }
+    
+        assertNotNull(boat); // Verify that a boat was found
+        String expected = "Aquarius Yacht";
+        String actual = boat.getBoatType().getName();
+        assertEquals(expected, actual);
     }
 
     @Test
     void CreateBoatAssemblyWithCustomerIsSucces() {
-        BoatType boatType = new BoatType("Test", 1, 0, "Slow");
-        BoatAssembly boatAssembly = new BoatAssembly();
-        boatAssembly.selectedBoat = new Boat(boatType);
-        boatAssembly.boatOfferte(new Customer());                                                                       
+        assertDoesNotThrow(() -> {
+            BoatType boatType = new BoatType("Test", 1, 0, "Slow");
+            BoatAssembly boatAssembly = new BoatAssembly();
+            boatAssembly.selectedBoat = new Boat(boatType);
+            boatAssembly.boatOfferte(new Customer());
+        });
     }
 
     @Test
     void CreateNewBoat() {
-        ArrayList<BoatType> boatTypes = new ArrayList<BoatType>();
-        MotorType Honda = new MotorType("Honda", 5, 500);
-        RoomType stateroon = new RoomType("Stateroom", 4, 1100);
-        SteeringWheelType wood = new SteeringWheelType("Wood", 3, 450);
-        ExhaustType doublePipe = new ExhaustType("Double Pipe", 2, 400);
-        ColorType pearlescent = new ColorType("Pearlescent", 2, 1000);
-        WaterType swimmingPool = new WaterType("Swimming Pool", 2, 4500);
-        FlagType medium = new FlagType("Medium", 2, 60);
-        AnchorType hook = new AnchorType("Hook", 2, 30);
+        assertDoesNotThrow(() -> {
+            ArrayList<BoatType> boatTypes = new ArrayList<BoatType>();
+            MotorType Honda = new MotorType("Honda", 5, 500);
+            RoomType stateroon = new RoomType("Stateroom", 4, 1100);
+            SteeringWheelType wood = new SteeringWheelType("Wood", 3, 450);
+            ExhaustType doublePipe = new ExhaustType("Double Pipe", 2, 400);
+            ColorType pearlescent = new ColorType("Pearlescent", 2, 1000);
+            WaterType swimmingPool = new WaterType("Swimming Pool", 2, 4500);
+            FlagType medium = new FlagType("Medium", 2, 60);
+            AnchorType hook = new AnchorType("Hook", 2, 30);
 
-        BoatType boatType = new BoatType("Tester", 1, 1500000, "Medium");
-        boatTypes.add(boatType);
-        boatType.AddMotor(Honda);
-        boatType.AddRoom(stateroon);
-        boatType.AddSteeringWheel(wood);
-        boatType.AddExhaust(doublePipe);
-        boatType.AddColor(pearlescent);
-        boatType.AddWater(swimmingPool);
-        boatType.AddFlag(medium);
-        boatType.AddAnchor(hook);
+            BoatType boatType = new BoatType("Tester", 1, 1500000, "Medium");
+            boatTypes.add(boatType);
+            boatType.AddMotor(Honda);
+            boatType.AddRoom(stateroon);
+            boatType.AddSteeringWheel(wood);
+            boatType.AddExhaust(doublePipe);
+            boatType.AddColor(pearlescent);
+            boatType.AddWater(swimmingPool);
+            boatType.AddFlag(medium);
+            boatType.AddAnchor(hook);
+        });
     }
 }
